28cdb2a88efa0f77ace9f9085678352f3fb5e216
Will <asampongg377@gmail.com>
2024-10-28 18:00:22 +0000

    Init

--- Car-dealer-website/src/App.jsx
+++ Car-dealer-website/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+
 import Home from './Pages/Home'
 
 function App() {
   
 
   return (
-    <div className='w-4/5 mx-auto'>
-      <Home />
+    <div className=''>
+        <Home />
     </div>
   )
 }
